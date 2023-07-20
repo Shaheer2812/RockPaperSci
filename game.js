07c607d0366a6818c697375f2c98e5a2d573100d
Shaheer <shaheerhumayun1@gmail.com>
2023-07-20 22:44:11 +0500

    completed the ui

--- game.js
+++ game.js
@@ -1,83 +1,87 @@
+let computer_score = 0,
+  player_score = 0;
+
 function getComputerChoice() {
-    const choice = ['Rock', 'Paper', 'Scissor'];
-    let x = Math.floor((Math.random() * 3));
-    return choice[x];
-
+  const choice = ["Rock", "Paper", "Scissor"];
+  let x = Math.floor(Math.random() * 3);
+  return choice[x];
 }
-
 //returns 1 if human wins, -1 if computer wins, and 0 if its a tie
 function playRound(player, computer) {
-    player = player.toLowerCase();
-    computer = computer.toLowerCase();
+  player = player.toLowerCase();
+  computer = computer.toLowerCase();
 
-    if (player == computer) {
-        console.log("It's a Tie!")
-        return 0
+  if (player == computer) {
+    return "It's a Tie!";
+    return 0;
+  }
+
+  if (player == "rock") {
+    if (computer == "paper") {
+      computer_score++;
+      return "Computer wins! Paper beats Rock";
+      return -1;
+    } else {
+      player_score++;
+      return "You win! Rock beats Scissors";
+      return 1;
     }
-
-    if (player == "rock") {
-
-        if (computer == "paper") {
-            console.log("Computer wins! Paper beats Rock")
-            return -1
-        }
-
-        else {
-            console.log("You win! Rock beats Scissors")
-            return 1
-        }
+  } else if (player == "paper") {
+    if (computer == "rock") {
+      player_score++;
+      return "You win! Paper beats Rock";
+      return 1;
+    } else {
+      computer_score++;
+      return "Computer wins! Scissor beats Paper";
+      return -1;
     }
-
-    else if (player == "paper") {
-        if (computer == "rock") {
-            console.log("You win! Paper beats Rock")
-            return 1
-        }
-
-        else {
-            console.log("Computer wins! Scissor beats Paper")
-            return -1
-        }
+  } else {
+    if (computer == "rock") {
+      computer_score++;
+      return `Computer wins! ${computer} beats ${player}`;
+      return -1;
+    } else {
+      player_score++;
+      return `You win! ${player} beats ${computer}`;
+      return 1;
     }
+  }
+}
+const div = document.createElement("div");
+div.classList.add("winner");
 
-    else {
-        if (computer =="rock") {
-            console.log(`Computer wins! ${computer} beats ${player}`)
-            return -1
-        }
-
-        else {
-            console.log(`You win! ${player} beats ${computer}`)
-            return 1
-        }
-    }
+function reset() {
+  computer_score = 0;
+  player_score = 0;
 }
 
-function game() {
+const div1 = document.createElement("div");
+div1.classList.add("div1");
+//adding an event listener to buttons
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.className);
+    comp = getComputerChoice();
+    console.log(comp);
+    div.innerText = playRound(button.className, comp);
+    console.log(div.innerText);
+    div1.textContent = "";
 
-    let computer_score = 0, player_score = 0
-    
-    for (let i = 0; i < 5; i++) {
-        choice_player = prompt("Enter you choice")
-        choice_computer = getComputerChoice()
+    div.innerText += `\nCOMPUTER ${computer_score} - ${player_score} YOU`;
+    if (computer_score == 5) {
+      div1.textContent = "COMPUTER WINS! GIT GUD";
+      div1.style.color = "red";
+      reset();
+    } else if (player_score == 5) {
+      div1.textContent = "YOU WIN";
+      div1.style.color = 'green'
+      reset();
+    }
+  });
+});
 
-        ans = playRound(choice_player, choice_computer)
-
-        if (ans == 1) {
-            player_score++
-        }
-        else if (ans == -1) {
-            computer_score++;
-        }
-    }
-
-    if (computer_score == player_score) {
-        console.log(`There is no Winner...`)
-    }
-    else if (computer_score > player_score) {
-        console.log(`Computer wins by a score of ${computer_score} to ${player_score}`)
-    }
-    else {
-        console.log(`Human wins by a score of ${player_score} to ${computer_score}`)
-    }
-}
+const body = document.querySelector("body");
+body.append(div1);
+body.appendChild(div);
